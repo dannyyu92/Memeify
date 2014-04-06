3cd887a234a7631061ba17ae9b20cbbd35774e6f
Danny Yu <dannyyu92@gmail.com>
2014-04-05 20:09:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Memeify::Application.config.secret_key_base = '661124cbcd2537a4d71dc2724ea6ae1ab47405623cdd012ab6e959bc946fa13b7a2c178afb9f8f581be297d8936cb3b2958641b2829ed57a58cf9380ed650a87'
